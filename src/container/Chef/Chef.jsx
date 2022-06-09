bd9d2cfa5c1ade373ddee083afb30e7d7f3c8506
FakharElyes <elyes.fakhar@esprit.tn>
2022-06-09 15:35:47 +0100

    chef

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -16,13 +16,13 @@ const Chef = () => (
       <div className="app__chef-content">
         <div className="app__chef-content_quote">
           <img src={images.quote} alt="quote_image" />
-          <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit auctor sit .</p>
+          <p className="p__opensans">I have spent more time in my life working and being in restaurants than being at home.</p>
         </div>
-        <p className="p__opensans"> auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit. Nulla scelerisque scelerisque congue ac consequat, aliquam molestie lectus eu. Congue iaculis integer curabitur semper sit nunc. </p>
+        <p className="p__opensans">I immediately feel comfortable entering a restaurant, and I feel even more comfortable in the back with the chef and cooks.</p>
       </div>
 
       <div className="app__chef-sign">
-        <p>Kevin Luo</p>
+        <p>Fakhar Elyes</p>
         <p className="p__opensans">Chef & Founder</p>
         <img src={images.sign} alt="sign_image" />
       </div>
