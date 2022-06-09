2ebd395589e63c8d335f506ab010aecfe910afe0
FakharElyes <elyes.fakhar@esprit.tn>
2022-06-09 15:38:36 +0100

    awards

--- src/constants/data.js
+++ src/constants/data.js
@@ -50,22 +50,12 @@ const awards = [
   {
     imgUrl: images.award02,
     title: 'Bib Gourmond',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+    subtitle: 'Restaurant and Bar Design Awards.',
   },
   {
     imgUrl: images.award01,
     title: 'Rising Star',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
-  },
-  {
-    imgUrl: images.award05,
-    title: 'AA Hospitality',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
-  },
-  {
-    imgUrl: images.award03,
-    title: 'Outstanding Chef',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+    subtitle: 'James Beard Award.',
   },
 ];
 
